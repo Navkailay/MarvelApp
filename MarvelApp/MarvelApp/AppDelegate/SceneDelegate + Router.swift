f0c5b354a7b84bd721daf4a22f4cbf1cff866bc5
Navpreet Kailay <navpreetkailay@gmail.com>
2022-10-28 23:39:15 +0530

    refactor(projectSetup): refactored code and extra code

---
+++ MarvelApp/MarvelApp/AppDelegate/SceneDelegate + Router.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate + Router.swift
+//  ManekTechTest
+//
+//  Created by Navpreet Kailay on 22/09/22.
+//
+
+import Foundation
+import UIKit
+
+extension SceneDelegate {
+    
+    func setRootController(_ windowScene: UIWindowScene) {
+     
+    }
+    
+    func setHomeRootController(_ windowScene: UIWindowScene){
+       
+    }
+
+}
